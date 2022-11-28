1943a167b475c6551a1e956f7eac115498bbf174
Lainey-Moffett <91988367+Lainey-Moffett@users.noreply.github.com>
2022-11-28 08:23:18 -0600

    Update levelOne.js

--- levelOne.js
+++ levelOne.js
@@ -3,9 +3,10 @@ let startButton = document.getElementById("btnOne");
 btnOne.onclick = function () {
     let flagPrompt = prompt("What is the Winter Grinch's password?")
     if ((flagPrompt == "33")){
-        window.location.href='https://lainey-moffett.github.io/Saving-Winter-/levelTwo.html';
-        else {
-        alert("Sorry, that is incorrect. Please try again!")
+        window.location.href='https://www.google.com/';
+
     }
+    else {
+        alert("Sorry, that is incorrect. Please try again!")
     }
 }
